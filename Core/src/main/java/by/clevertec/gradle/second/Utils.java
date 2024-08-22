c7dae2369fd7164c06fa5787414417bfca4de452
Andrei Vasilyev <andrei.vasilyev80@gmail.com>
2024-08-22 23:38:36 +0300

    added modules core and api

---
+++ Core/src/main/java/by/clevertec/gradle/second/Utils.java
@@ -0,0 +1,13 @@
+package by.clevertec.gradle.second;
+
+import java.util.Arrays;
+
+public class Utils {
+
+    public boolean isAllPositiveNumbers(String... str) {
+
+
+        return true;
+
+    }
+}
